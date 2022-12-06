3564077226f0a6b9f72c0d63ef4dbfd5d9ba6d29
Yussuph Ibitoye <ibitoyeolawale@gmail.com>
2022-12-05 19:30:02 -0800

    Task 5 mod

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     new_str = ""
     for char in my_string:
-        if char  != 'c' and char != 'C':
+        if char != 'c' and char != 'C':
             new_str += char
     return new_str
