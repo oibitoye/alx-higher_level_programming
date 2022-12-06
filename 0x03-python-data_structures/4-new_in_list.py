41ce662220f108fd9adb07f61d6d0a34af1c071d
Yussuph Ibitoye <ibitoyeolawale@gmail.com>
2022-12-05 19:05:07 -0800

    Task 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    new_list = []
+    if idx < 0 or idx >= my_list.size():
+        return my_list
+    else:
+        for i in my_list:
+            if i == idx:
+                new_list.append(element)
+            else:
+                new_list.append(i)
+    return new_list
