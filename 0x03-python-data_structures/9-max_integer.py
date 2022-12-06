9ae8d1060cc18c9d5cf72d05322f3bd14d89cbc0
Yussuph Ibitoye <ibitoyeolawale@gmail.com>
2022-12-05 20:31:19 -0800

    9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    len_list = len(my_list)
+
+    if len_list == 0:
+        return (None)
+    max_num = my_list[0]
+    for i in range(1, length):
+        if max_num < my_list[i]:
+            max_num = my_list[i]
+
+    return (max_num)
