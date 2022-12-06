09cb85fb3166eecd0862e359252f30e532298951
Yussuph Ibitoye <ibitoyeolawale@gmail.com>
2022-12-05 19:50:24 -0800

    Task 7

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    len_a = len(tuple_a), len_b = len(tuple_b)
+
+    if len_a == 0:
+        a_one = 0, a_two = 0
+    elif len_a == 1:
+        a_one = tuple_a[0], a_two = 0
+    else:
+        a_one = tuple_a[0], a_two = tuple_a[1]
+
+    if len_b == 0:
+        b1 = 0, b2 = 0
+    elif len_b == 1:
+        b1 = tuple_b[0], b2 = 0
+    else:
+        b1 = tuple_b[0], b2 = tuple_b[1]
+
+    tuple_new = (a_one + b1, a_two + b2)
+
+    return (tuple_new)
