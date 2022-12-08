b5f4a714329d54625282abfa6e5e73a475be52e2
Yussuph Ibitoye <ibitoyeolawale@gmail.com>
2022-12-07 17:42:25 -0800

    Task 100

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list is not None:
+        numb = 0
+        density = 0
+        for i in my_list:
+            numb = numb + (i[0] * i[1])
+            density = density + i[1]
+        weight = (numb / density)
+        return weight
+    return 0
