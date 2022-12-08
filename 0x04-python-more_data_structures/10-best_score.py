6d726e2f6d274d96caa6b9c5881c97484e9dd487
Yussuph Ibitoye <ibitoyeolawale@gmail.com>
2022-12-07 17:09:41 -0800

    Task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if len(a_dictionary):
+        high_score = 0
+        high_key = ""
+        for key in a_dictionary:
+            if a_dictionary[key] > high_score:
+                high_score = a_dictionary[key]
+                high_key = key
+        return high_key
+    else:
+        return None
