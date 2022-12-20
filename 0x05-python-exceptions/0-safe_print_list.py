bd2d73716dbf83a67fd8ea3619ed1135a5894ff5
Yussuph Ibitoye <ibitoyeolawale@gmail.com>
2022-12-19 19:17:45 -0800

    Task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
         for i in my_list:
             if x > pos:
                 print("{}".format(i), end="")
-                pos = pos + 1;
+                pos = pos + 1
         print("")
     except SyntaxError:
         print("Syntax Error Occured")
