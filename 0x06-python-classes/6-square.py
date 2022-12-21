cb9319f30ddb2edbe2f0fc74b56357024f36e929
Yussuph Ibitoye <ibitoyeolawale@gmail.com>
2022-12-20 20:00:06 -0800

    Task 6

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+Task 4-scuare.py
+This is a Class Square with private instance attrib
+"""
+
+
+class Square:
+    """
+    Class Square with private instance attrib size
+    and Public instance method: area(self)
+    """
+    def __init__(self, size=0):
+        self.__size = size
+        if type(self.__size) is not int:
+            raise TypeError("size must be an integer")
+        elif self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        self.__size = size
+        if type(self.__size) is not int:
+            raise TypeError("size must be an integer")
+        elif self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if (len(value) != 2 or not isinstance(value, tuple) or
+            not all(i >=0 for i in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+            return
+
+        for i in range(0, self.__position[1]):
+            print()
+        
+        for i in range(0, self.__size):
+            for j in range(0, self.__position[0]):
+                print(" ", end="")
+            for k in range(0, self.__position[0]):
+                print("#", end="")
+            print()
