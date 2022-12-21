64f502f89d261cde077aafcd5e36ddda70a14d81
Yussuph Ibitoye <ibitoyeolawale@gmail.com>
2022-12-20 19:40:15 -0800

    Task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Task 4-scuare.py
+This is a Class Square with private instance attrib
+"""
+
+
+class Square:
+    """
+    Class Square with private instance attrib size
+    and Public instance method: area(self)
+    """
+    def __init__(self, size=0):
+        self.__size = size
+        if type(self.__size) is not int:
+            raise TypeError("size must be an integer")
+        elif self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        self.__size = size
+        if type(self.__size) is not int:
+            raise TypeError("size must be an integer")
+        elif self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        elif self.__size > 0:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
