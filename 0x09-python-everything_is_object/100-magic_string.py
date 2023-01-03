ded247a490a753f7fc4e585148f0709b1a2ba9a9
Yussuph Ibitoye <ibitoyeolawale@gmail.com>
2023-01-03 07:38:33 -0800

    Task 100

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string(cnt=[0]):
-    cnt[0]+= 1
-    return (str("BestSchool, " * (cnt[0]-1)) + "BestSchool")
+    cnt[0] += 1
+    return (str("BestSchool, " * (cnt[0] - 1)) + "BestSchool")
