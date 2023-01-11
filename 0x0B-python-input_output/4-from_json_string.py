7325f770762d00e1e8eb90c756c15fd10335fd6f
Yussuph Ibitoye <ibitoyeolawale@gmail.com>
2023-01-10 18:17:58 -0800

    task 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" 
+filename: 4-from_json_string.py
+function that returns an object
+(Python data structure) represented by a JSON string
+"""
+import json
+
+
+def from_json_string(my_str):
+    """ 
+    function that returns an object
+    (Python data structure) represented by a JSON string
+    """
+    return json.loads(my_str)
