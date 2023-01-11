06888e57e96404024dc4de2b7bbfbdbbe27da4ed
Yussuph Ibitoye <ibitoyeolawale@gmail.com>
2023-01-10 18:17:30 -0800

    task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" 
+filename: 1-write_file.py
+function that writes a string to a text file (UTF8)
+and returns the number of characters written:
+"""
+
+
+def write_file(filename="", text=""):
+    """ 
+    function that writes a string to a text file (UTF8)
+    """
+
+    with open(filename, 'w', encoding="utf-8") as file:
+        return file.write(text)
