ae4e74994bb2194caa5e0aaf70f08a4e2effa3e8
Yussuph Ibitoye <ibitoyeolawale@gmail.com>
2023-01-10 18:17:40 -0800

    task 2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" 
+filename: 2-append_write.py
+function that appends a string at the end of a text
+file (UTF8) and returns the number of characters added
+"""
+
+
+def append_write(filename="", text=""):
+    """ 
+    function that appends a string at the end of a text
+    file (UTF8) and returns the number of characters added
+    """
+
+    with open(filename, 'a', encoding="utf-8") as file:
+        return file.write(text)
