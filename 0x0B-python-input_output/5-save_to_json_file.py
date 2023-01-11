8e5f226ce395402491286cad6a28b846c119d898
Yussuph Ibitoye <ibitoyeolawale@gmail.com>
2023-01-10 18:18:08 -0800

    task 5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" 
+filename: 5-save_to_json_file.py
+function that writes an Object to a
+text file, using a JSON representation:
+"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """ 
+    function that writes an Object to a
+    text file, using a JSON representation:
+    """
+    with open(filename, 'w', encoding="utf-8") as file:
+        json.dump(my_obj, file)
