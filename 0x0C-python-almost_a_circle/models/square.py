2657909a45701f3c99defd37e76d6594a9b03ad3
Yussuph Ibitoye <ibitoyeolawale@gmail.com>
2023-01-20 20:35:19 -0800

    Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
-""" Module that contains class Square,
-inheritance of class Rectangle
+"""This Module contains class Square,
+an inheritance of the class Rectangle
 """
-from models.rectangle import Rectangle
+from models.rectangle import Rectangle as Rect
 
 
-class Square(Rectangle):
-    """ Class Rectangle """
+class Square(Rect):
+    """ Inherit Class Rectangle """
 
     def __init__(self, size, x=0, y=0, id=None):
-        """ Initializes instances """
+        """ Initializes Square instances """
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """ str special method """
-        str_square = "[Square] "
-        str_id = "({}) ".format(self.id)
-        str_xy = "{}/{} - ".format(self.x, self.y)
-        str_wh = "{}/{}".format(self.width, self.height)
-
-        return str_square + str_id + str_xy + str_wh
+        return (
+            f"[Square] ({self.id}) {self.x}/{self.y} "
+            f"- {self.width}/{self.height}"
+        )
 
     @property
     def size(self):
@@ -34,16 +32,13 @@ class Square(Rectangle):
 
     def __str__(self):
         """ str special method """
-        str_rectangle = "[Square] "
-        str_id = "({}) ".format(self.id)
-        str_xy = "{}/{} - ".format(self.x, self.y)
-        str_size = "{}".format(self.size)
-
-        return str_rectangle + str_id + str_xy + str_size
+        return (
+            f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
+        )
 
     def update(self, *args, **kwargs):
         """ update method """
-        if args is not None and len(args) is not 0:
+        if args is not None and len(args) > 0:
             list_atr = ['id', 'size', 'x', 'y']
             for i in range(len(args)):
                 if list_atr[i] == 'size':
