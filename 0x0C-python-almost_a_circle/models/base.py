e2ebc2fe8fcdf14a0eae93d83b6955330421bc8b
Yussuph Ibitoye <ibitoyeolawale@gmail.com>
2023-01-20 19:54:33 -0800

    Base corrected

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -14,7 +14,7 @@ class Base:
             self.id = id
         else:
             Base.__nb_objects += 1
-            id = Base.__nb_objects
+            self.id = Base.__nb_objects
 
     @staticmethod
     def to_json_string(list_dictionaries):
