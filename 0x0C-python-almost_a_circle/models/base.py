6bcdf928b0bff5473bee5e87061e120d4887dfdb
Yussuph Ibitoye <ibitoyeolawale@gmail.com>
2023-01-20 19:52:50 -0800

    Base corrected

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -6,15 +6,15 @@ import os.path
 
 
 class Base:
-    """Base class for all other classes"""
+    """This is the Base class for all classes"""
     __nb_objects = 0
 
     def __init__(self, id=None):
         if id is not None:
             self.id = id
         else:
-            Base().__nb_objects += 1
-            id = Base().__nb_objects
+            Base.__nb_objects += 1
+            id = Base.__nb_objects
 
     @staticmethod
     def to_json_string(list_dictionaries):
